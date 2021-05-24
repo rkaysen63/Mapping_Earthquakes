439d2fbe0132f48a7a0934478fc96b2dc4e8d59f
rkaysen63 <rkaysen@plant-process.com>
2021-05-23 22:03:59 -0500

    map a single point on branch

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -5,7 +5,9 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // Create tile layer that will be background of the map. https://leafletjs.com/examples/quick-start/
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// mapbox://styles/mapbox/streets-v11
+// mapbox://styles/mapbox/dark-v10
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -13,6 +15,15 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 // Add 'graymap' tile layer to the map.
 streets.addTo(map);
 
+//  Add a marker to the map for Los Angeles, California.
+// var marker = L.marker([34.0522, -118.2437]).addTo(map);
 
+//  Add a circle marker to the map for Los Angeles, California.
+var marker = L.circle([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: '#ffffa1',
+    fillOpacity: 0.5,
+    radius: 300
+ }).addTo(map);
 
 
