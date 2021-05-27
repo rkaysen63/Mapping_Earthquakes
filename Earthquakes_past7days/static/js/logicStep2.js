6d5da5ec710d3e304e4af56a1dd85eaa4b3347ce
rkaysen63 <rkaysen@plant-process.com>
2021-05-27 18:17:04 -0500

    added style and radius functions

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -46,9 +46,61 @@ L.control.layers(baseMaps).addTo(map);
 // Retrieve the USGS Earthquake GeoJSON URL.
 let earthquakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+// This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+    return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: "#ffae42",
+    color: "#000000",
+    radius: getRadius(),
+    stroke: true,
+    weight: 0.5
+    };
+}
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+}
+
 // Grab GeoJSON data.
 d3.json(earthquakeData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {
+    // Turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+        console.log(data);
+        return L.circleMarker(latlng);
+    },
+    // Set the style for each circleMarker using the styleInfo function
+    style: styleInfo
+  }).addTo(map);
 });
+
+
+
+// Grab GeoJSON data.
+// d3.json(earthquakeData).then(function(data) {
+    // console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  // L.geoJson(data, {
+    // https://www.w3schools.com/colors/colors_picker.asp   
+      // color: "#ff00ff", //hot pink
+      // weight: 2,
+      // fillColor: "#b3ffff", //aqua
+      // fillOpacity: .5,
+    // We turn each feature into a marker on the map.
+    // onEachFeature: function(feature, layer) { 
+        // console.log(layer);
+        // layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME +  
+        // "</h3>");
+    // }
+  // }).addTo(map);
+// });
