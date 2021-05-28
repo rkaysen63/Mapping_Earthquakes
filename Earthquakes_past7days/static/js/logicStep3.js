649e92a7a1242b02aff94c4352a6c88db1cc260a
rkaysen63 <rkaysen@plant-process.com>
2021-05-27 23:09:55 -0500

    added layers.js for tile variables

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -1,31 +1,24 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create tile layer that will be background of the map. https://leafletjs.com/examples/quick-start/
-// https://docs.mapbox.com/api/maps/styles/
-// mapbox://styles/mapbox/streets-v11
-// mapbox://styles/mapbox/dark-v10
-// mapbox://styles/mapbox/light-v10
-// mapbox://styles/mapbox/navigation-night-v1
-// mapbox://styles/mapbox/navigation-day-v1
-// mapbox://styles/mapbox/outdoors-v11
-// mapbox://styles/mapbox/satellite-streets-v11
 // Create street view tile layer that will be default background of the map.
-// Define variables for our tile layers
-// Create the light tile layer that will be an option for the map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: "streets-v11",
-    accessToken: API_KEY
-});
+// Layers.js has variables for : streets, outdoors, light, dark,
+// satellite, satelliteStreets, navigationDay, navigationNight
 
-// Create the dark view tile layer that will be an option for the map.
-let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+// Create the streets tile layer that will be an option for the map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    // attribution: 'Map data © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    // maxZoom: 18,
+    // id: "streets-v11",
+    // accessToken: API_KEY
+// });
+
+// Create the satelliteStreets view tile layer that will be an option for the map.
+// let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    // attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    // maxZoom: 18,
+    // accessToken: API_KEY
+// });
 
 // Create a base layer that holds both maps.
 let baseMaps = {
--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -1,31 +1,16 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create tile layer that will be background of the map. https://leafletjs.com/examples/quick-start/
-// https://docs.mapbox.com/api/maps/styles/
-// mapbox://styles/mapbox/streets-v11
-// mapbox://styles/mapbox/dark-v10
-// mapbox://styles/mapbox/light-v10
-// mapbox://styles/mapbox/navigation-night-v1
-// mapbox://styles/mapbox/navigation-day-v1
-// mapbox://styles/mapbox/outdoors-v11
-// mapbox://styles/mapbox/satellite-streets-v11
-// Create street view tile layer that will be default background of the map.
-// Define variables for our tile layers
-// Create the light tile layer that will be an option for the map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: "streets-v11",
-    accessToken: API_KEY
+// Create the map object with center, zoom level, and default layer.
+let map = L.map('mapid', {
+  center: [39.5, -98.5], //Center of U.S.
+  zoom: 3,
+  layers: [streets] //default layer
 });
 
-// Create the dark view tile layer that will be an option for the map.
-let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+// Create street view tile layer that will be default background of the map.
+// Layers.js has variables for : streets, outdoors, light, dark,
+// satellite, satelliteStreets, navigationDay, navigationNight
 
 // Create a base layer that holds both maps.
 let baseMaps = {
@@ -33,15 +18,17 @@ let baseMaps = {
     "Streets": streets
 };
 
-// Create the map object with center, zoom level, and default layer.
-let map = L.map('mapid', {
-    center: [39.5, -98.5], //Center of U.S.
-    zoom: 3,
-    layers: [streets] //default layer
-});
+// Create the earthquake layer for the map.
+let earthquakes = new L.layerGroup();
+
+// Define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
 
 // Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Retrieve the USGS Earthquake GeoJSON URL.
 let earthquakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
@@ -107,5 +94,8 @@ d3.json(earthquakeData).then(function(data) {
     onEachFeature: function(feature, layer) {
         layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
     }
-  }).addTo(map);
+  }).addTo(earthquakes);
+
+  // Add earthquake layer to the map.
+  earthquakes.addTo(map);
 });
